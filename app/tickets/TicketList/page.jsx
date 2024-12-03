dd8cf7630af96e29da659082bc50f1dedcdd2f81
KavinT06 <itskavin47@gmail.com>
2024-12-03 21:18:22 +0530

    modifications in fetch

--- app/tickets/TicketList/page.jsx
+++ app/tickets/TicketList/page.jsx
@@ -1,8 +1,8 @@
 async function getTickets() {
     const res = await fetch('http://localhost:4000/tickets', {
-        next: {
-            revalidate: 0 // use 0 to opt out of using cache
-        }
+        // next: {
+            // revalidate: 0 // use 0 to opt out of using cache
+        // }
     })
 
     return res.json()
